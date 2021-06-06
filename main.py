1dd48759c5b4d1e8ecdf3e647a92b02c2a36ea57
Mark Oulitin <fragoric@gmail.com>
2021-06-06 11:09:32 +0300

    Added Second split

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ def idxToCsv(idx, name):
     df.to_csv(f'{name}_idx.csv')
 
 
-def partitionToIdx():
-    idx, test_idx = next(ShuffleSplit(n_splits=1, test_size=0.4).split(np.arange(8189)))
-    train_idx, validation_idx = train_test_split(idx, test_size=0.5)
+def partitionToIdx(test_size, validation_size):
+    idx, test_idx = next(ShuffleSplit(n_splits=1, test_size=test_size).split(np.arange(8189)))
+    train_idx, validation_idx = train_test_split(idx, test_size=validation_size)
     idxToCsv(train_idx, 'train')
     idxToCsv(validation_idx, 'validation')
     idxToCsv(test_idx, 'test')
 
 
-def readIdx(file):
+def csvReadIdx(file):
     df = pd.read_csv(file)
     idx = np.array(df.iloc[:, 1])
     return idx
 
 
 def main():
-    pass
+    partitionToIdx(0.1, 1/9)
 
 
 if __name__ == '__main__':
